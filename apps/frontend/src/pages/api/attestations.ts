9e4ae44325c3fdcf74f3f288ef78c76bcee32887
liayoo <lia.yoo@smartcontract.com>
2023-07-14 21:34:27 +0900

    Implement attestations api

--- apps/frontend/src/pages/api/attestations.ts
+++ apps/frontend/src/pages/api/attestations.ts
@@ -1,4 +1,14 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import Axios from "axios";
+import web3 from "web3";
+
+const CCTP_API_ENDPOINT_TESTNET = 'https://iris-api-sandbox.circle.com/v1/attestations/';
+// TODO: distinguish between testnet vs mainnet
+const CCTP_API_ENDPOINT_MAINNET = 'https://iris-api.circle.com/v1/attestations/';
+
+const CCTP_API_STATUS_COMPLETE = "complete";
+
+const axios = Axios.create();
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,7 +19,22 @@ export default async function handler(
     console.log("data:", data);
     console.log("sender:", sender);
 
-    // TODO: parse message from data, hash it, and get attestations for it
-    // 'https://iris-api-sandbox.circle.com/attestations/'
+    const msgHash = web3.utils.keccak256(data);
+    console.log("msgHash:", msgHash);
+    return await axios.get(CCTP_API_ENDPOINT_TESTNET + msgHash)
+      .then((response) => {
+        console.log("response:", response);
+        if (response.data.status == CCTP_API_STATUS_COMPLETE) {
+          return res.json(response.data.attestation);
+        } else {
+          return res.status(404).json(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log("error:", error);
+        return res.status(error?.response.status).send(error?.data?.error);
+      })
+  } else {
+    return res.status(404).send('Invalid method');
   }
 }
